a78cb7c1cbf67a43bf85de68cfbeb40f3d705bb2
Joshua Ntow Opare-Boateng <109949311+jnopareboateng@users.noreply.github.com>
2024-01-31 17:04:26 +0000

    Exercise L3

--- 01_Day_JavaScript_Refresher/exerciseL3.js
+++ 01_Day_JavaScript_Refresher/exerciseL3.js
@@ -1,7 +1,31 @@
 const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24];
-const sortedAges = ages.sort();
-const minAge = sortedAges.min();
-const maxAge = sortedAges.max();
-const medianAge = 0; 
+const sortedAges = ages.sort((a, b) => a - b); // sort in ascending order
+const minAge = Math.min(...sortedAges);
+const maxAge = Math.max(...sortedAges);
+let medianAge; 
+let meanAge;
 
-for (i = 0; i= )
+function calculateMeanAge(){
+    let sum = 0;
+    for (let i = 0; i < sortedAges.length; i++){
+        sum += sortedAges[i];
+    }
+    meanAge = sum / sortedAges.length;
+}
+
+function calculateMedianAge(){
+    const mid = Math.floor(sortedAges.length / 2);
+    if (sortedAges.length % 2 == 0){
+        medianAge = (sortedAges[mid - 1] + sortedAges[mid]) / 2;
+    } else {
+        medianAge = sortedAges[mid];
+    }
+}
+
+calculateMeanAge();
+calculateMedianAge();
+
+console.log(`Min Age: ${minAge}`);
+console.log(`Max Age: ${maxAge}`);
+console.log(`Mean Age: ${meanAge}`);
+console.log(`Median Age: ${medianAge}`);
