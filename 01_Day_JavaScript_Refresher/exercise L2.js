bb57bccd095898e872b714ea4ed45e13c0eb3db9
Joshua Ntow Opare-Boateng <109949311+jnopareboateng@users.noreply.github.com>
2024-01-31 16:48:24 +0000

    L2 Exercise completed

--- 01_Day_JavaScript_Refresher/exercise L2.js
+++ 01_Day_JavaScript_Refresher/exercise L2.js
@@ -40,3 +40,10 @@ function checkSass(){
   }
 }
 
+// Question 6
+const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+const backEnd = ['Node', 'Express', 'MongoDB']
+
+const fullStack = frontEnd.concat(backEnd);
+
+console.log(fullStack)
